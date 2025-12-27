6542839d574579777e8918d26a8143d7010ebd3a
yashvandre <rockyash155@gmail.com>
2025-12-27 23:50:58 +0530

    04: background jobs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { TRPCReactProvider } from "@/trpc/client";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -68,6 +69,7 @@ export default function RootLayout({
       >
         {/* ✅ PROVIDER MUST BE INSIDE BODY */}
         <TRPCReactProvider>
+          <Toaster/>
           {children}
         </TRPCReactProvider>
       </body>
