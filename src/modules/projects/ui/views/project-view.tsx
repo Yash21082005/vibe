e7b52ede0a00167d85c4651570361d1bc58b8a36
yashvandre <rockyash155@gmail.com>
2026-01-04 19:26:41 +0530

    14: home page

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -41,7 +41,7 @@ const [tabState,setTabState] = useState<"preview" | "code">("preview");
                  />
                 </Suspense>
         </ResizablePanel>
-        <ResizableHandle withHandle/>
+        <ResizableHandle className="hover:bg-primary transition-colors"/>
         <ResizablePanel
         defaultSize={65}
         minSize={50}>
